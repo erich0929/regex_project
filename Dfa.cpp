f96ba60d41212df5d8b5b308cff1f6239c45ed01
erich0929 <erich0929@gmail.com>
2014-10-16 18:11:59 +0900

    clean comments.

--- Dfa.cpp
+++ Dfa.cpp
@@ -10,24 +10,13 @@ SingleWordNode::SingleWordNode(UCHAR* from, UCHAR* to, bool IsNotExp, bool IsRan
 {
 		unsigned int length = 0;
 		
-		/* form  
-		length = strlen ((char*)from);
-		this -> from = (UCHAR*) malloc (sizeof (UCHAR) * (length + 1));
-		strncpy ((char*)this -> from, (char*)from, length + 1);
-		 */
 		this -> from = new Ebuf (from);
-		/* to 
-		length = strlen ((char*)to);
-		this -> to = (UCHAR*) malloc (sizeof (UCHAR) * (length + 1));
-		strncpy ((char*)this -> to, (char*)to, length + 1);
-		*/
+		
 		this -> to = new Ebuf (to);
-		/* m_length */
-		//this -> m_length = length;
+	
 		/* IsNotExp */
 		this -> IsNotExp = IsNotExp;
 		/* IsUTF8 */
-		std::cout << this -> from -> getLength () << std::endl;
 		this -> IsUTF8 = (((this -> from -> getLength ()) == 1) ? false : true) ; 
 		
 		this -> IsRange = IsRange;
@@ -45,21 +34,6 @@ SingleWordNode::SingleWordNode (Ebuf* from ,Ebuf* to, bool IsNotExp, bool IsRang
 }
 
 SingleWordNode::SingleWordNode(SingleWordNode& source) {
-		
-		//unsigned int length = 0;
-		/* form 
-		length = strlen ((char*)source.from);
-		this -> from = (UCHAR*) malloc (sizeof (UCHAR) * (length + 1));
-		strncpy ((char*)this -> from, (char*)source.from, length + 1);
-		 */
-		/* 
-		 * to 
-		length = strlen ((char*)source.to);
-		this -> to = (UCHAR*) malloc (sizeof (UCHAR) * (length + 1));
-		strncpy ((char*)this -> to, (char*)source.to, length + 1);
-		 */
-		/* m_length */
-		//this -> m_length = source.m_length;
 		this -> from = new Ebuf (*source.from);
 		this -> to = new Ebuf (*source.to);
 		/* IsNotExp */
@@ -79,34 +53,14 @@ int SingleWordNode::match(Ebuf& ebuf, OFFSET startOffset, Ebuf& lazyChar) const
 		UCHAR current = ebuf.getNext(startOffset);
 		bool IsAccept = true;
 		
-		/* ASCII Range Mode */
-		/*
-		if (!(this -> IsUTF8)) {
-				std::cout << "Not UTF8" << std::endl;
-				if (current >= from -> getNext (0) && current <= to -> getNext (0)) IsAccept = true;
-				IsNotExp ? IsAccept = !IsAccept : IsAccept;
-				
-				// if IsAccept and IsLazyChar , then return 0 
-				if (IsAccept && current == lazyChar.getNext(0)) {
-						std::cout << "lazyChar accepted" << std::endl;
-						return 0;
-				}
-				return IsAccept ? 1 : -1;
-		} else {
-		*/
-				/* case : Mutibytes and just 'from' char (string) to be used. */
+		
+	
 				int i = 0;
 				OFFSET acceptedOffset = 0;
 				bool IsLazyAccepted = true;
 				unsigned int lazyCount = 0;
 				unsigned int charSize = 0;
-				/**
-				if (from -> getNext (0) == '\0' && IsNotExp == true && current <= 128) {
-					charSize = 1; /* ASCII MODE 
-				} else {
-					charSize = from -> getLength ();
-				} /* Mutibytes Mode 
-				**/
+				
 				
 				if (IsNotExp) {
 					/* criterion to buffer */
@@ -116,19 +70,18 @@ int SingleWordNode::match(Ebuf& ebuf, OFFSET startOffset, Ebuf& lazyChar) const
 					charSize = from -> getLength ();
 					
 				}
-				std::cout << "charsize :" << charSize << std::endl;
-				for (i = 0; /*i < this -> from -> getLength ()*/(i < charSize) && (IsAccept) ; i++) {
+				//std::cout << "charsize :" << charSize << std::endl;
+				for (i = 0; (i < charSize) && (IsAccept) ; i++) {
 						current = ebuf.getNext (startOffset + i);
 						
 						if (this -> IsRange) {
-							printf ("from : %d, to : %d, current : %d\n", from -> getNext (0), to -> getNext (0), current);
+							//printf ("from : %d, to : %d, current : %d\n", from -> getNext (0), to -> getNext (0), current);
 							(current >= from -> getNext (0) && current <= to -> getNext (0)) ? IsAccept = true : IsAccept = false;
 						} else {
 							(current == from -> getNext (i)) ? IsAccept = true : IsAccept = false; 
 						}
 						
-						if ((current == lazyChar.getNext(lazyCount)) && IsLazyAccepted
-								/*&& IsAccept*/) {
+						if ((current == lazyChar.getNext(lazyCount)) && IsLazyAccepted) {
 									lazyCount ++;
 						} else {
 								IsLazyAccepted = false;
@@ -136,23 +89,19 @@ int SingleWordNode::match(Ebuf& ebuf, OFFSET startOffset, Ebuf& lazyChar) const
 				}
 				
 				IsNotExp ? IsAccept = !IsAccept : IsAccept;
-						std::cout << "let's determine. : " << IsAccept << std::endl;
 						if (IsAccept) {
 								acceptedOffset += charSize;
 						} else {
 								std::cout << IsAccept << std::endl;
 								return -1;
 						}
-				/* if IsAccept and IsLazyChar , then return 0 */
 				if (IsLazyAccepted && IsAccept) {
-						std::cout << "is lazy" << std::endl;
+						//std::cout << "is lazy" << std::endl;
 						return 0;
 				} else {
-						std::cout << "is not lazy" << std::endl;
+						//std::cout << "is not lazy" << std::endl;
 						return acceptedOffset;
 				}
-				/*
-		}*/
 }
 
 DfaNode* SingleWordNode::clone () {
@@ -256,18 +205,7 @@ int StarNode::match (Ebuf& ebuf, OFFSET startOffset, Ebuf& lazyChar) const {
 		OFFSET lazyOffset = 0;
 		OFFSET lazyAccepted = 0;
 		UCHAR lazyCurrent;
-		
-		/*
-		while ((lazyCurrent = lazyChar.getNext(lazyOffset)) != '\0') {
-				current = ebuf.getNext (startOffset + acceptedOffset + lazyOffset);
-				if (current == lazyCurrent) {
-						lazyAccepted += 1;
-				} else {
-						return -1;
-				}
-				lazyOffset ++;
-		}
-		*/
+	
 		SingleWordNode lazyToken (&lazyChar, &lazyChar, false, false);
 		Ebuf nullToken ((UCHAR*) "");
 		if ((lazyAccepted = lazyToken.match (ebuf, startOffset + acceptedOffset, nullToken)) < 0) return -1;
